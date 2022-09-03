c7e289b54523a9d81611909c944d61974b7cf4f6
antonkazakov <antonanton08@gmail.com>
2022-09-03 23:30:20 +0300

    Добавил задание по Activity#3

---
+++ README.md
@@ -0,0 +1,6 @@
+# Activities #3
+
+Домашняя работа к лекции **Activities #3**. В ней мы научимся работать с Runtime Permissions и Result Api
+
+Задание лежит здесь:
+https://kazakovanton.notion.site/Activity-3-2d041e4a120b4d0bb51ba7c73417bdb9
---
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -0,0 +1,44 @@
+package otus.gpb.homework.activities
+
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+        imageView = findViewById(R.id.imageview_photo)
+
+        findViewById<Toolbar>(R.id.toolbar).apply {
+            inflateMenu(R.menu.menu)
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.send_item -> {
+                        openSenderApp()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+    }
+
+    /**
+     * Используйте этот метод чтобы отобразить картинку полученную из медиатеки в ImageView
+     */
+    private fun populateImage(uri: Uri) {
+        val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
+        imageView.setImageBitmap(bitmap)
+    }
+
+    private fun openSenderApp() {
+        TODO("В качестве реализации метода отправьте неявный Intent чтобы поделиться профилем. В качестве extras передайте заполненные строки и картинку")
+    }
+}
